eabd784ed641a37c6ffc1885131df4c2f1e0d1c0
Md <md1040582@gmail.com>
2023-12-23 08:22:32 +0600

    some code update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
-
+import MainView from './components/view/MainView';
+import { ToastContainer } from 'react-toastify';
 function App() {
   return (
     <div className="App">
+      <MainView/>
+      <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+/>
     </div>
   );
 }
